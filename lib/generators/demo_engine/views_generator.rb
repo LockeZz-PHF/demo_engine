65a71480f236e35a232f1873515dffcf840ad0f6
LockeZz <LockeZz.PHF@gmail.com>
2025-04-09 02:20:44 +0800

    test2 for view generator

--- lib/generators/demo_engine/views_generator.rb
+++ lib/generators/demo_engine/views_generator.rb
@@ -1,15 +1,18 @@
 require 'rails/generators/base'
 
 module DemoEngine
-    module Generators 
+  module Generators 
 
-        class ViewsGenerator < Rails::Generators::Base
-            desc "Copy DemoEngine Views to host application"
+    class ViewsGenerator < Rails::Generators::Base
+      extend ActiveSupport::Concern
+      source_root File.expand_path("../../../../app/views", __FILE__)
+      desc "Copy DemoEngine Views to host application"
+      hide!
 
-            def copy_views
-                directory "app/views", "app/views/demo_engine"
-            end
-
-        end
+      def copy_views
+        view_directory :demo_engine
+      end
     end
+
+  end
 end
